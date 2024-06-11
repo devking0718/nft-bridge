194cc49ea762b8cbfb30e39956b318ce88d48389
Alex <devking0718@gmail.com>
2024-06-11 23:15:10 +0900

    fix issue with nft receiving

--- contracts/BridgeSender.sol
+++ contracts/BridgeSender.sol
@@ -4,9 +4,10 @@ pragma solidity 0.8.19;
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BridgeSender is Ownable {
+contract BridgeSender is Ownable, IERC721Receiver {
     struct MessageInfo {
         address nftCollection;
         uint64 tokenId;
@@ -30,12 +31,33 @@ contract BridgeSender is Ownable {
         uint256 serviceFee
     );
 
-    constructor(
-        address _router,
-        uint64 _destChain
-    ) {
+    event NFTLocked(
+        address indexed owner,
+        address indexed collectionAddress,
+        uint256 tokenId
+    );
+
+    constructor(address _router, uint64 _destChain) {
         ROUTER = IRouterClient(_router);
         DEST_CHAIN = _destChain;
+    }
+
+    function lockNFT(address collectionAddress, uint256 tokenId) private {
+        IERC721(collectionAddress).safeTransferFrom(
+            msg.sender,
+            address(this),
+            tokenId
+        );
+        emit NFTLocked(msg.sender, collectionAddress, tokenId);
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public pure override returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 
     function addCollection(address nftCollection) external onlyOwner {
@@ -99,13 +121,13 @@ contract BridgeSender is Ownable {
         );
 
         // Send the message through the router and store the returned message ID
-        messageId = ROUTER.ccipSend(DEST_CHAIN, evm2AnyMessage);
-
-        IERC721(nftCollection).safeTransferFrom(
-            msg.sender,
-            address(this),
-            tokenId
+        messageId = ROUTER.ccipSend{value: bridgeFee}(
+            DEST_CHAIN,
+            evm2AnyMessage
         );
+
+        // Lock NFT by sending NFT to this
+        lockNFT(nftCollection, tokenId);
 
         uint256 restFee = msg.value - bridgeFee - serviceFee;
         if (restFee > 0) {
